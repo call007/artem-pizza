ca5e0dca211049bc8f7f508697e79a3d8fa525ee
e.shevtsov <e.shevtsov@unitedtraders.com>
2021-10-09 21:25:35 +0300

    fix: remove broken button

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ function App() {
   return (
     <div className="App">
       <PizzaConfigurator />
-      <button
-        onClick={() => {
-          throw new Error("Some test error");
-        }}
-      >
-        Break the world
-      </button>
-      ;
     </div>
   );
 }
