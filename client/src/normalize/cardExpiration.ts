04fd4e56abac9e675435873799876ab050eb9d82
e.shevtsov <e.shevtsov@unitedtraders.com>
2021-11-03 00:22:09 +0300

    feat: separate app to admin and client

--- client/src/normalize/cardExpiration.ts
+++ client/src/normalize/cardExpiration.ts
@@ -1,7 +1,7 @@
-export const cardExpirationRegExp = /^[0-9]{2}\/[0-9]{4}$/;
+export const cardExpirationRegExp = /^[0-3][0-9]\/[0-9]{4}$/;
 
 export const isThisACardExpiration = (candidate: string) =>
-  cardExpirationRegExp.test(candidate);
+  candidate.replace(/[^0-9]/g, "").length === 6;
 
 export const normalizeCardExpiration = (value: string) => {
   return value.replace(/[^0-9]/g, "").replace(/^(\d{2})(\d+)/, "$1/$2");
