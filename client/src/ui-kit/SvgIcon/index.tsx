3d3025ddcffc33065a451f8a0b919b245fb04268
e.shevtsov <e.shevtsov@unitedtraders.com>
2021-12-29 16:44:02 +0300

    feat: ui-kit component SvgIcon

---
+++ client/src/ui-kit/SvgIcon/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { ReactComponent as AccountIcon } from "./icons/account.svg";
+import { ReactComponent as ArrowLeftIcon } from "./icons/arrow-left.svg";
+import { ReactComponent as CheckIcon } from "./icons/check.svg";
+import { ReactComponent as DeliveryIcon } from "./icons/delivery.svg";
+import { ReactComponent as ErrorIcon } from "./icons/error.svg";
+import { ReactComponent as LogoutIcon } from "./icons/logout.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as RepeatIcon } from "./icons/repeat.svg";
+import * as Styled from "./styles";
+
+export type SvgSrc =
+  | "arrow-left"
+  | "check"
+  | "plus"
+  | "account"
+  | "repeat"
+  | "delivery"
+  | "error"
+  | "logout";
+
+export interface SvgIconProps {
+  src: SvgSrc;
+  size?: number;
+}
+
+const icons: {
+  [key in SvgSrc]: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+} = {
+  "arrow-left": ArrowLeftIcon,
+  check: CheckIcon,
+  plus: PlusIcon,
+  account: AccountIcon,
+  repeat: RepeatIcon,
+  delivery: DeliveryIcon,
+  error: ErrorIcon,
+  logout: LogoutIcon,
+};
+
+export function SvgIcon({ size = 16, src }: SvgIconProps) {
+  return <Styled.Container size={size} as={icons[src]} aria-hidden="true" />;
+}
---
+++ client/src/ui-kit/SvgIcon/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+type ContainerProps = {
+  size: number;
+};
+
+export const Container = styled.svg<ContainerProps>`
+  flex-shrink: 0;
+  vertical-align: middle;
+  width: ${(props) => `${props.size / 16}rem`};
+  height: ${(props) => `${props.size / 16}rem`};
+  fill: currentColor;
+  transform: translateZ(0);
+`;
