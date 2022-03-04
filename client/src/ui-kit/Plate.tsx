1e794a20b94807a8adc5195eb04379a33c299d45
e.shevtsov <e.shevtsov@unitedtraders.com>
2022-03-04 19:37:17 +0300

    fix: color transition

--- client/src/ui-kit/Plate.tsx
+++ client/src/ui-kit/Plate.tsx
@@ -5,6 +5,5 @@ export const Plate = styled.div`
   background-color: ${(props) => props.theme.colors.white};
   box-shadow: ${(props) => props.theme.shadow.outer.lg};
   border-radius: 1rem;
-  transition-property: background-color, color;
-  transition-duration: ${variables.transitionDuration};
+  transition: background-color ${variables.transitionDuration};
 `;
